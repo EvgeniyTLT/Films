573474e4fcc324f79d7a791315f3a43ce40e257c
EvgeniyTLT <Toy444@ya.ru>
2022-08-24 01:03:31 +0300

    add

--- app/src/main/java/com/example/films/Movie.kt
+++
@@ -1,4 +0,0 @@
-package com.example.films
-
-data class Movie(var title: String, var image: String)
-
---
+++ app/src/main/java/com/example/films/TestingDataClass.kt
@@ -0,0 +1,6 @@
+package com.example.films
+
+data class TestingDataClass(
+    val `data`: Data,
+    val support: Support
+)
