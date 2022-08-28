1b52ca67491838b048048ff0606f83c1fae0a709
EvgeniyTLT <Toy444@ya.ru>
2022-08-28 22:41:57 +0300

    add

--- app/src/main/java/com/example/films/CustomAdapter.kt
+++ app/src/main/java/com/example/films/CustomAdapter.kt
@@ -20,9 +20,7 @@ class CustomAdapter(private val mList: List<Result>?) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val ItemsViewModel = mList?.get(position)
         Picasso.get().load("https://image.tmdb.org/t/p/w500"+mList?.get(position)?.poster_path).into(holder.imageView)
-
-
-    }
+   }
     override fun getItemCount(): Int {
         return mList!!.size
     }
